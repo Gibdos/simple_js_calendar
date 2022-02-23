cf2f8a7051c6adc11b787f0fb3297c63c0d6e0a9
Gibdos <simon.yousefi@protonmail.com>
2022-02-23 08:41:11 +0100

    default to current month, +reset btn

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const ulDays = document.querySelector('ul');
 const currentDate = new Date();
 const currentDay = currentDate.getDate();
 const currentMonth = currentDate.toLocaleString('en', { month: 'long' });
+const selectedMonth = currentMonth;
+let days;
 
-selectMonth.value = 'January'
+selectMonth.value = currentMonth;
+selectionHeader.innerHTML = currentMonth;
+if (selectedMonth === 'February') {
+    createCalendar('28', selectedMonth)
+}
+else if (selectedMonth === 'April' || selectedMonth === 'June' || selectedMonth === 'September' || selectedMonth === 'November') {
+    createCalendar('30', selectedMonth)
+} else {
+    createCalendar('31', selectedMonth)
+}
+
 
 selectMonth.addEventListener('change', () => {
     const selectedMonth = selectMonth.value;
-
     let days = 31;
     if (selectedMonth === 'February') {
         days = 28;
@@ -36,5 +47,28 @@ function createCalendar(days, selectedMonth) {
     }
 }
 
-createCalendar(31, 'January')
+if (currentMonth === selectedMonth) {
+    const callback = element => element.innerHTML == currentDay;
+    const elements = Array.from(document.getElementsByTagName('li'));
+    const result = elements.filter(callback);
+    result[0].style.cssText = 'background-color: #5d323b; border: 1px solid white;';
+    }
+
+function resetCalendar() {
+    selectMonth.value = currentMonth;
+    selectionHeader.innerHTML = currentMonth;
+    if (selectedMonth === 'February') {
+        days = 28;
+    }
+    else if (selectedMonth === 'April' || selectedMonth === 'June' || selectedMonth === 'September' || selectedMonth === 'November') {
+        days = 30;
+    }
+    createCalendar(days, selectedMonth);
+    if (currentMonth === selectedMonth) {
+        const callback = element => element.innerHTML == currentDay;
+        const elements = Array.from(document.getElementsByTagName('li'));
+        const result = elements.filter(callback);
+        result[0].style.cssText = 'background-color: #5d323b; border: 1px solid white;';
+        }
+}
 // APRIL, JUNE, SEPT, NOV = 30
